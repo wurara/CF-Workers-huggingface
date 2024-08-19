6d730af8f72ec054f287ac89e1bc4d593c901a1c
吴小东 <49011440+wurara@users.noreply.github.com>
2024-08-19 17:02:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    url.host = 'api.openai.com';
+    url.host = 'chat.openai.com';
     return fetch(url, { headers: request.headers, method: request.method, body: request.body });
   },
 };

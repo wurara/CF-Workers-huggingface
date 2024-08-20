14d1a7c1c021e97626c908da31ff6e0e224ff16c
吴小东 <49011440+wurara@users.noreply.github.com>
2024-08-20 10:19:26 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,15 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    request.headers.set('x-forwarded-for', '172.16.19.233');
-    request.headers.set('x-real-ip', '172.16.19.233');
-    request.headers.set('remote_addr', '172.16.19.233');
+    
+    // 复制原始请求的标头
+    const headers = new Headers(request.headers);
+    // 确保 Host 头部被替换为 hub.docker.com
+    headers.set('Host', 'registry.hub.docker.com');
+    headers.set('x-forwarded-for', '172.16.19.233');
+    headers.set('x-real-ip', '172.16.19.233');
+    headers.set('remote_addr', '172.16.19.233');
+    
     url.host = 'chat.openai.com';
     return fetch(url, { headers: request.headers, method: request.method, body: request.body });
   },

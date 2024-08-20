202d2bfa13daca76a64f04abb08e65ae82946e1c
吴小东 <49011440+wurara@users.noreply.github.com>
2024-08-20 09:04:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,10 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    url.host = 'poe.com';
+    request.headers.set('x-forwarded-for', '172.16.19.233');
+    request.headers.set('x-real-ip', '172.16.19.233');
+    request.headers.set('remote_addr', '172.16.19.233');
+    url.host = 'chat.openai.com';
     return fetch(url, { headers: request.headers, method: request.method, body: request.body });
   },
 };
